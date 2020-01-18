9dd7c2a687705216eb90c60a330ef4e7b42046af
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-01-18 20:04:34 +0100

    deletion of an old leetcode dir

--- leetcode/binarySearch/SearchInsertPosition_35/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] arr = {1,3};
-        System.out.println(solution.searchInsert(arr, 4));
-    }
-}
