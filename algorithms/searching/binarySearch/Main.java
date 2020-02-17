5f0a23b665d13280fad1456b6dca1786cfdeef1c
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-02-17 22:40:47 +0100

    Updated binarySearch with 3 templates

--- algorithms/searching/binarySearch/Main.java
+++ algorithms/searching/binarySearch/Main.java
@@ -3,7 +3,8 @@
 class BinarySearch {
     // Time O(log n)
     // Space O(1)
-    public int binarySearch(int[] arr, int x) {
+    // Template1
+    public int binarySearch1(int[] arr, int x) {
         int low = 0;
         int high = arr.length - 1;
         int mid;
@@ -17,6 +18,46 @@ class BinarySearch {
                 return mid;
             }
         }
+        return -1;
+    }
+
+    // Gurantees Search Space is at least 2 in size at each step
+    public int binarySearch2(int[] nums, int target) {
+        int left = 0, right = nums.length;
+        while(left < right){
+            // Prevent (left + right) overflow
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < target) { 
+                left = mid + 1; 
+            }
+            else { 
+                right = mid;   
+            }
+        }
+      
+        // Post-processing:
+        // End Condition: left == right
+        if(left != nums.length && nums[left] == target) return left;
+        return -1;
+    }
+
+    // Gurantees Search Space is at least 3 in size at each step
+    public int binarySearch3(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left + 1 < right){
+            // Prevent (left + right) overflow
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+    
+        // Post-processing:
+        // End Condition: left + 1 == right
+        if(nums[left] == target) return left;
+        if(nums[right] == target) return right;
         return -1;
     }
 
@@ -39,6 +80,6 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {2,4,6,8,10,11};
         var bs = new BinarySearch();
-        System.out.println(bs.binarySearchRecursive(arr, 8, 0, arr.length - 1));
+        System.out.println(bs.binarySearch3(arr, 8));
     }
 }
