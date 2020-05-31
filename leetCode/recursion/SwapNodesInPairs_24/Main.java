511d0e0eb1bc23f982d84754575d5b12b4b64fa2
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-05-31 15:33:04 +0200

    Solved Swap

---
+++ leetCode/recursion/SwapNodesInPairs_24/Main.java
@@ -0,0 +1,38 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // base case
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode node = head.next;
+        head.next = swapPairs(node.next);
+        node.next = head;
+        return node;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        a.next = b;
+        ListNode c = new ListNode(3);
+        b.next = c;
+        ListNode d = new ListNode(4);
+        c.next = d;
+        d.next = null;
+
+        var s = new Solution();
+        var swap = s.swapPairs(a);
+
+        int x = 3;
+    }
+}
