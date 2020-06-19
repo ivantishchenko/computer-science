30946b48647dbec681ad701098ee9476f3257f06
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-06-19 22:59:36 +0200

    Solved Perfect squares

---
+++ leetCode/queue/Perfect_Squares_279/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public int numSquares(int n) {
+        if(n == 0) return 0;
+        if(Math.sqrt(n) % 1 == 0) return 1;
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        q.offer(0);
+        int numEdges = 1;
+        while(!q.isEmpty()) {
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                int curr = q.poll();
+                if(visited.contains(curr)) continue;
+                visited.add(curr);
+                for(int j = 1; j <= n; j++) {
+                    int next = curr + j;
+                    if(Math.sqrt(j) % 1 == 0) {
+                        if(next < n) {
+                            q.offer(next);
+                        } else if(next == n) {
+                            return numEdges;
+                        }  
+                    } 
+                }
+            }
+            numEdges++;
+        }
+        return numEdges;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        var s = new Solution();
+        s.numSquares(12);
+    }
+}
