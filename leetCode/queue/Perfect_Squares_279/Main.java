f428f8ca61d90629f0c5982fb7036b7eb22d35e2
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-06-19 23:24:57 +0200

    Updated perfect squares 279

--- leetCode/queue/Perfect_Squares_279/Main.java
+++ leetCode/queue/Perfect_Squares_279/Main.java
@@ -8,21 +8,17 @@ class Solution {
         Set<Integer> visited = new HashSet<>();
         q.offer(0);
         int numEdges = 1;
+        int upperBound = (int) Math.sqrt(n);
         while(!q.isEmpty()) {
             int qSize = q.size();
             for (int i = 0; i < qSize; i++) {
                 int curr = q.poll();
                 if(visited.contains(curr)) continue;
                 visited.add(curr);
-                for(int j = 1; j <= n; j++) {
-                    int next = curr + j;
-                    if(Math.sqrt(j) % 1 == 0) {
-                        if(next < n) {
-                            q.offer(next);
-                        } else if(next == n) {
-                            return numEdges;
-                        }  
-                    } 
+                for(int j = 1; j <= upperBound; j++) {
+                    int next = curr + j * j;
+                    if(next < n) q.offer(next);
+                    else if(next == n) return numEdges;
                 }
             }
             numEdges++;
