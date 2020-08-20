8275cee3a1ad7995367cf18f174aafeddad6c90b
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-08-20 16:41:47 +0200

    Implemented Queue using 2 Stacks

---
+++ leetCode/queue/Implement_Queue_using_Stacks_232/main.py
@@ -0,0 +1,50 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+
+        tmp_stack = []
+
+        for _ in range(len(self.stack)):
+            tmp_stack.append(self.stack.pop())
+
+        res = tmp_stack.pop()
+
+        for _ in range(len(tmp_stack)):
+            self.stack.append(tmp_stack.pop())
+
+        return res
+
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.stack[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.stack
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
