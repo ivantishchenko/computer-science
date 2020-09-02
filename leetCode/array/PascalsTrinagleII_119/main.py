40877a39343d9419f8f7890ff583229a60cf36bb
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-02 21:18:49 +0200

    Minor mod

--- leetCode/array/PascalsTrinagleII_119/main.py
+++ leetCode/array/PascalsTrinagleII_119/main.py
@@ -1,9 +1,9 @@
 class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
+    def getRow(self, rowIndex):
         prev_row = [1]
         if not rowIndex:
             return prev_row
-        for i in range(rowIndex + 1):
+        for i in range(1, rowIndex + 1):
             curr_row = [1]
 
             for j in range(i - 1):
