1c66a06b0f7840840f56787fd1bcccec3b5ebf9e
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-07 20:36:36 +0200

    Solved search a 2D matrix with O(n ^ 2)

---
+++ leetCode/recursion/Search_a_2D_Matrix_II_240/main.py
@@ -0,0 +1,37 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+
+        Naive O(n ^ 2)
+
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+        if len(matrix) == 1 and len(matrix[0]) == 1:
+            if matrix[0][0] == target:
+                return True
+            else:
+                return False
+
+        pivot_x, pivot_y = len(matrix) // 2, len(matrix[0]) // 2
+
+        tl = self.searchMatrix([row[:pivot_y] for row in matrix[:pivot_x]], target)
+        br = self.searchMatrix([row[pivot_y:] for row in matrix[pivot_x:]], target)
+        bl = self.searchMatrix([row[pivot_y:] for row in matrix[:pivot_x]], target)
+        tr = self.searchMatrix([row[:pivot_y] for row in matrix[pivot_x:]], target)
+
+        return tl or tr or bl or br
+
+
+a = Solution()
+res = a.searchMatrix([
+    [1, 4, 7, 11, 15],
+    [2, 5, 8, 12, 19],
+    [3, 6, 9, 16, 22],
+    [10, 13, 14, 17, 24],
+    [18, 21, 23, 26, 30]
+], 20)
+print(res)
