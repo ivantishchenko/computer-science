38a0b0eb9041beae3c39bfc182755b5bd135054e
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-08 11:25:26 +0200

    Reduced search space to 3 matrices

--- leetCode/recursion/Search_a_2D_Matrix_II_240/main.py
+++ leetCode/recursion/Search_a_2D_Matrix_II_240/main.py
@@ -18,6 +18,39 @@ class Solution:
 
         pivot_x, pivot_y = len(matrix) // 2, len(matrix[0]) // 2
 
+        if matrix[pivot_x][pivot_y] > target:
+            tl = self.searchMatrix([row[:pivot_y] for row in matrix[:pivot_x]], target)
+            bl = self.searchMatrix([row[pivot_y:] for row in matrix[:pivot_x]], target)
+            tr = self.searchMatrix([row[:pivot_y] for row in matrix[pivot_x:]], target)
+            return tl or tr or bl
+        elif matrix[pivot_x][pivot_y] < target:
+            br = self.searchMatrix([row[pivot_y:] for row in matrix[pivot_x:]], target)
+            bl = self.searchMatrix([row[pivot_y:] for row in matrix[:pivot_x]], target)
+            tr = self.searchMatrix([row[:pivot_y] for row in matrix[pivot_x:]], target)
+            return tr or bl or br
+        else:
+            return True
+
+
+    def searchMatrix_bf(self, matrix, target):
+        """
+
+        Naive O(n ^ 2)
+
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+        if len(matrix) == 1 and len(matrix[0]) == 1:
+            if matrix[0][0] == target:
+                return True
+            else:
+                return False
+
+        pivot_x, pivot_y = len(matrix) // 2, len(matrix[0]) // 2
+
         tl = self.searchMatrix([row[:pivot_y] for row in matrix[:pivot_x]], target)
         br = self.searchMatrix([row[pivot_y:] for row in matrix[pivot_x:]], target)
         bl = self.searchMatrix([row[pivot_y:] for row in matrix[:pivot_x]], target)
@@ -33,5 +66,5 @@ res = a.searchMatrix([
     [3, 6, 9, 16, 22],
     [10, 13, 14, 17, 24],
     [18, 21, 23, 26, 30]
-], 20)
+], 5)
 print(res)
