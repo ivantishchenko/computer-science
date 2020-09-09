8d3735324fa3d0b8835e9f8b86797c628716e452
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-09 17:11:40 +0200

    Solved N Queens I

---
+++ leetCode/recursion/N-Queens_51/main.py
@@ -0,0 +1,38 @@
+class Solution:
+    def place_queen(self, diag1, diag2, cols, row, col):
+        diag1.add(row + col)
+        diag2.add(row - col)
+        cols[col] = row
+
+    def remove_queen(self, diag1, diag2, cols, row, col):
+        diag1.remove(row + col)
+        diag2.remove(row - col)
+        del cols[col]
+
+    def is_under_attack(self, diag1, diag2, cols, row, col):
+        return row + col in diag1 or row - col in diag2 or col in cols
+
+    def build_res(self, cols, n):
+        res = [n * [] for _ in range(n)]
+
+        for col in cols:
+            res[cols[col]] = max(col, 0) * '.' + 'Q' + (n - 1 - col) * '.'
+
+        return res
+
+    def backtrack(self, diag1, diag2, cols, n, row, res):
+        for col in range(n):
+            if not self.is_under_attack(diag1, diag2, cols, row, col):
+                self.place_queen(diag1, diag2, cols, row, col)
+                if row == n - 1:
+                    res.append(self.build_res(cols, n))
+                else:
+                    self.backtrack(diag1, diag2, cols, n, row + 1, res)
+                self.remove_queen(diag1, diag2, cols, row, col)
+        return res
+
+    def solveNQueens(self, n):
+        diag_set1 = set()
+        diag_set2 = set()
+        col_map = {}
+        return self.backtrack(diag_set1, diag_set2, col_map, n, 0, [])
