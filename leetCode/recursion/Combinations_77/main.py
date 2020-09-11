8d99b6c3acbfe8cc84afc129e6137d5808af2325
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-11 15:59:37 +0200

    Solved Combinations with recursion

---
+++ leetCode/recursion/Combinations_77/main.py
@@ -0,0 +1,22 @@
+class Solution:
+    def helper(self, candidates, k):
+        if k == 0:
+            return []
+        if k == 1:
+            return [[c] for c in candidates]
+        res = []
+        for i in range(len(candidates)):
+            next_combinations = self.helper(candidates[i + 1:], k - 1)
+            for c in next_combinations:
+                res.append([candidates[i]] + c)
+        return res
+
+    def combine(self, n, k):
+        candidates = list(range(1, n + 1))
+        res = self.helper(candidates, k)
+        return res
+
+
+solver = Solution()
+out = solver.combine(4, 2)
+print(out)
