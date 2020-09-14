333adc9525f4d6cfe7f92534e2da7a801c82a86f
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-14 12:44:09 +0200

    Implemented buggy quick select

--- leetCode/heap/Top_K_Frequent_Elements_347/main.py
+++ leetCode/heap/Top_K_Frequent_Elements_347/main.py
@@ -1,8 +1,42 @@
 import heapq
 from collections import Counter
+import random
 
 
 class Solution:
+    def parition(self, start, end, freq, keys_list):
+        pivot_index = random.randint(start, end)
+        pivot = freq[keys_list[pivot_index]]
+        keys_list[pivot_index], keys_list[end] = keys_list[end], keys_list[pivot_index]
+
+        j = start
+        for i in range(start, end):
+            if freq[keys_list[i]] <= pivot:
+                keys_list[j], keys_list[i] = keys_list[i], keys_list[j]
+                j += 1
+
+        keys_list[j], keys_list[end] = keys_list[end], keys_list[j]
+        return j
+
+    def quick_select(self, start, end, freq, keys_list, k):
+        if start < end:
+            pivot_idx = self.parition(start, end, freq, keys_list)
+            if pivot_idx > k:
+                self.quick_select(pivot_idx + 1, end, freq, keys_list, k)
+            elif pivot_idx < k:
+                self.quick_select(start, pivot_idx - 1, freq, keys_list, k)
+            else:
+                return
+
+    # quick_select
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        keys_list = list(freq.keys())
+
+        n = len(keys_list)
+        self.quick_select(0, n - 1, freq, keys_list, n - k)
+        return keys_list[n - k:]
+
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # O(1) time
         if k == len(nums):
