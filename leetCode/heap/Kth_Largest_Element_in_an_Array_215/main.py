7da1673013913da4a23f61fb446e174318eb0846
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-14 19:34:44 +0200

    Implemented k-th largest with quickSelect

---
+++ leetCode/heap/Kth_Largest_Element_in_an_Array_215/main.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+
+        def partition(start, end):
+            j = start
+            pivot = nums[end]
+            for i in range(start, end):
+                if nums[i] <= pivot:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    j += 1
+
+            nums[end], nums[j] = nums[j], nums[end]
+            return j
+
+        def quick_select(start, end, k_idx):
+            if start < end:
+                pivot_idx = partition(start, end)
+                if pivot_idx > k_idx:
+                    quick_select(start, pivot_idx - 1, k_idx)
+                elif pivot_idx < k_idx:
+                    quick_select(pivot_idx + 1, end, k_idx)
+                else:
+                    return
+
+        quick_select(0, n - 1, n - k)
+        return nums[n - k]
