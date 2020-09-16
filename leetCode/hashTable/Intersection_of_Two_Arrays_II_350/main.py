67c2e2bbc9e9b207a523655176306a118eac9e80
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-16 14:55:55 +0200

    Solved intersection of arrays 2

---
+++ leetCode/hashTable/Intersection_of_Two_Arrays_II_350/main.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        c1 = Counter(nums1)
+        c2 = Counter(nums2)
+        res = set(nums1) & set(nums2)
+
+        final = []
+        for el in res:
+            final.extend(min(c1[el], c2[el]) * [el])
+
+        return final
