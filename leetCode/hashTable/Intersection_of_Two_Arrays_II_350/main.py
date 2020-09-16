a5a55b3c883b414c28a2e1ded2decaeba9762159
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-16 14:57:21 +0200

    2 liner for intersection

--- leetCode/hashTable/Intersection_of_Two_Arrays_II_350/main.py
+++ leetCode/hashTable/Intersection_of_Two_Arrays_II_350/main.py
@@ -2,6 +2,11 @@ from collections import Counter
 
 
 class Solution:
+    def intersect(self, nums1, nums2):
+        a, b = map(collections.Counter, (nums1, nums2))
+        print((a & b))
+        return list((a & b).elements())
+
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         c1 = Counter(nums1)
         c2 = Counter(nums2)
