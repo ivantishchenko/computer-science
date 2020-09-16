b4f1ca7a2f032cecd30f3204855a0070a50fa292
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-16 13:14:20 +0200

    Implemented a two heap approach for MeadianFinder

--- leetCode/heap/Find_Median_from_Data_Stream_295/main.py
+++ leetCode/heap/Find_Median_from_Data_Stream_295/main.py
@@ -1,4 +1,4 @@
-import random
+import heapq
 
 class MedianFinder:
 
@@ -6,42 +6,21 @@ class MedianFinder:
         """
         initialize your data structure here.
         """
-        self.data = []
+        self.min_heap = []
+        self.max_heap = []
 
     def addNum(self, num: int) -> None:
-        self.data.append(num)
+        heapq.heappush(self.max_heap, -num)
+        heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
 
-    def partition(self, start, end):
-        j = start
-        pivot = self.data[end]
-        for i in range(start, end):
-            if self.data[i] <= pivot:
-                self.data[i], self.data[j] = self.data[j], self.data[i]
-                j += 1
-        self.data[end], self.data[j] = self.data[j], self.data[end]
-        return j
-
-    def quick_select(self, start, end, k_idx):
-        if start < end:
-            pivot_idx = self.partition(start, end)
-            if pivot_idx > k_idx:
-                self.quick_select(start, pivot_idx - 1, k_idx)
-            elif pivot_idx < k_idx:
-                self.quick_select(pivot_idx + 1, end, k_idx)
-            else:
-                return
+        if len(self.max_heap) < len(self.min_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
 
     def findMedian(self) -> float:
-        # quick select N / 2
-        n = len(self.data)
-        k = n // 2
-        self.quick_select(0, n - 1, k)
-
-        if len(self.data) % 2 == 1:
-            return self.data[k]
+        if len(self.max_heap) == len(self.min_heap):
+            return (self.min_heap[0] - self.max_heap[0]) / 2
         else:
-            return (self.data[k - 1] + self.data[k]) / 2
-
+            return -self.max_heap[0]
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
 # obj.addNum(num)
