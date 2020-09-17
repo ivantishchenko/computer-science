ccac46ff4edf995e43568c51b3c766166263c383
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-17 16:25:13 +0200

    Added a NK solution for the group anagrams

--- leetCode/hashTable/Group_Anagrams_49/main.py
+++ leetCode/hashTable/Group_Anagrams_49/main.py
@@ -1,6 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_map = {}
+        for word in strs:
+            key = [0] * 26
+            for letter in word:
+                key[ord(letter) - ord('a')] += 1
+            key = tuple(key)
+            if key not in anagram_map:
+                anagram_map[key] = [word]
+            else:
+                anagram_map[key].append(word)
+        return list(anagram_map.values())
+
+    def groupAnagrams_sort(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = {}
         for w in strs:
             key = "".join(sorted(w))
             if key not in anagram_map:
