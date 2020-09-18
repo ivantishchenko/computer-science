48d69815e95c1fcab879b173f6d2fde62e053ac6
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-18 21:10:40 +0200

    Added a hack with a flag to pass longest substring with n^2

--- leetCode/hashTable/Longest_Substring_Without_Repeating_Characters_3/main.py
+++ leetCode/hashTable/Longest_Substring_Without_Repeating_Characters_3/main.py
@@ -3,12 +3,19 @@ class Solution:
         return len(s) == len(set(s))
 
     def lengthOfLongestSubstring(self, s):
-        for w_size in range(len(s), 0, -1):
+        if len(s) <= 1:
+            return len(s)
+
+        for w_size in range(1, len(s) + 1, 1):
+            sub_found = False
             for i in range(len(s)):
                 if i + w_size > len(s):
                     continue
 
                 if self.is_unique(s[i:i + w_size]):
-                    return w_size
+                    sub_found = True
 
-        return 0
+            if not sub_found:
+                return w_size - 1
+
+        return len(s)
