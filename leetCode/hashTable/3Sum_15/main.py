96155504f8fbe4160c75bb55ddb5a1cbd036cc12
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-22 18:32:00 +0200

    Alternative solution to 3SUM using two pointers

--- leetCode/hashTable/3Sum_15/main.py
+++ leetCode/hashTable/3Sum_15/main.py
@@ -1,4 +1,18 @@
 class Solution:
+    def two_sum_bin(self, nums, target, res):
+        i, j = 0, len(nums) - 1
+        while i < j:
+            curr_val = nums[i] + nums[j]
+            if curr_val > target:
+                j -= 1
+            elif curr_val < target:
+                i += 1
+            else:
+                res.add((-target, nums[i], nums[j]))
+                i += 1
+                j -= 1
+        return
+
     def two_sum(self, nums, target, res):
         # two sum
         sum_set = set()
@@ -12,6 +26,6 @@ class Solution:
         res = set()
         n = len(nums)
         for i in range(n):
-            self.two_sum(nums[i + 1:], -nums[i], res)
+            self.two_sum_bin(nums[i + 1:], -nums[i], res)
 
         return res
