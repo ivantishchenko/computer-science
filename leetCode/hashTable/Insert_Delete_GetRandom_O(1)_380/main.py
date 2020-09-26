3d55e64b81efd76fdf5b7fb9a5e443cef2c52dab
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-26 13:44:52 +0200

    Solved O(n) time get random

---
+++ leetCode/hashTable/Insert_Delete_GetRandom_O(1)_380/main.py
@@ -0,0 +1,43 @@
+import random
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = set()
+
+    def insert(self, val: int) -> bool:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val not in self.data:
+            self.data.add(val)
+            return True
+        else:
+            return False
+
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.data:
+            self.data.remove(val)
+            return True
+        else:
+            return False
+
+    def getRandom(self) -> int:
+        """
+        Get a random element from the set.
+        """
+        data_list = list(self.data)
+        return random.choice(data_list)
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
