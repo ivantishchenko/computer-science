8a7b3383cc0ced706aba6ea231919b79dcd34830
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-26 15:16:13 +0200

    Implemented 99% insert delete random Set with hash_map + arr

--- leetCode/hashTable/Insert_Delete_GetRandom_O(1)_380/main.py
+++ leetCode/hashTable/Insert_Delete_GetRandom_O(1)_380/main.py
@@ -1,39 +1,45 @@
 import random
 
-
 class RandomizedSet:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.data = set()
+        self.data_arr = []
+        self.data_map = {}
 
     def insert(self, val: int) -> bool:
         """
         Inserts a value to the set. Returns true if the set did not already contain the specified element.
         """
-        if val not in self.data:
-            self.data.add(val)
-            return True
-        else:
+        if val in self.data_map:
             return False
+
+        self.data_arr.append(val)
+        self.data_map[val] = len(self.data_arr) - 1
+
+        return True
+
 
     def remove(self, val: int) -> bool:
         """
         Removes a value from the set. Returns true if the set contained the specified element.
         """
-        if val in self.data:
-            self.data.remove(val)
-            return True
-        else:
+        if val not in self.data_map:
             return False
+
+        self.data_map[self.data_arr[-1]] = self.data_map[val]
+        self.data_arr[self.data_map[val]], self.data_arr[-1] = self.data_arr[-1], self.data_arr[self.data_map[val]]
+        del self.data_map[val]
+        self.data_arr.pop()
+        return True
 
     def getRandom(self) -> int:
         """
         Get a random element from the set.
         """
-        return random.choice(list(self.data))
+        return random.choice(self.data_arr)
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
