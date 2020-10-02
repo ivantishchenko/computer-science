4d6adbc2c385fe57543ea8e283bfc266322a761f
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-10-02 16:45:33 +0200

    Cleaned up DP code

--- leetCode/dynamicProgramming/Longest_Increaseing_Subsequence_300/main.py
+++ leetCode/dynamicProgramming/Longest_Increaseing_Subsequence_300/main.py
@@ -1,17 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-
         if n == 0:
             return 0
-
-        dp = [1] + (n - 1) * [0]
-        res = 1
+        dp = n * [1]
         for i in range(1, n):
-            max_val = 0
             for j in range(i):
                 if nums[j] < nums[i]:
-                    max_val = max(max_val, dp[j])
-            dp[i] = max_val + 1
-            res = max(res, dp[i])
-        return res
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
