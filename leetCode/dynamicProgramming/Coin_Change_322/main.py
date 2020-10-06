d6fd941146eff2f6b73f76df53f9467b0ef700d5
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-10-06 20:28:53 +0200

    Improved coin change memoization from dictionary to array

--- leetCode/dynamicProgramming/Coin_Change_322/main.py
+++ leetCode/dynamicProgramming/Coin_Change_322/main.py
@@ -1,28 +1,26 @@
 import sys
 
 class Solution:
-    def __init__(self):
-        self.memo = {}
-
-    def helper(self, coin_sum, coins):
+    def helper(self, coin_sum, coins, memo):
         if coin_sum == 0:
             return 0
         if coin_sum < 0:
             return sys.maxsize
 
-        if coin_sum in self.memo:
-            return self.memo[coin_sum]
+        if memo[coin_sum - 1] != 0:
+            return memo[coin_sum - 1]
 
         res = sys.maxsize
         for c in coins:
-            res = min(res, self.helper(coin_sum - c, coins) + 1)
+            res = min(res, self.helper(coin_sum - c, coins, memo) + 1)
 
-        self.memo[coin_sum] = res
+        memo[coin_sum - 1] = res
         return res
 
 
     def coinChange(self, coins, amount):
-        res = self.helper(amount, coins)
+        memo = [0] * amount
+        res = self.helper(amount, coins, memo)
         return -1 if res == sys.maxsize else res
 
 a = Solution()
